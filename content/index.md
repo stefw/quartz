9871ffd1744d07a63d8bcdd35d549c1a3b83c65e
stefw <sw@magicmorning.net>
2025-02-22 22:45:48 +0100

    robo

--- content/index.md
+++ content/index.md
@@ -1,7 +1,7 @@
 ---
 title: "Titre de la note"
 ---
-	
+
 # Bienvenue sur mon site
 
 Ceci est ma page d'accueil. Vous pouvez naviguer à travers mes notes à partir d'ici.
--- quartz.config.ts
+++ quartz.config.ts
@@ -23,8 +23,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Roboto",
+        body: "Roboto",
         code: "IBM Plex Mono",
       },
       colors: {
